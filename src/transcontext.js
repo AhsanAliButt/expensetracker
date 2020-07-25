cd1e8255dc7cfa73468b569e5403f96f4a822a2f
ahaanbutt <60073494+ahaanbutt@users.noreply.github.com>
2020-07-25 21:03:18 +0500

    update

--- src/transcontext.js
+++ src/transcontext.js
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import React, { createContext, useReducer } from "react";
+import TransactionReducer from './transReducer';
 
 const initialTransactions = [
 {amount:500 , desc:"Cash"},
@@ -6,3 +7,31 @@ const initialTransactions = [
 {amount:-150 , desc:"EasyLoad"}
 ]
 export const TransactionContext = createContext(initialTransactions);
+
+export const TransactionProvider =({children})=>{
+let [state,dispatch] = useReducer(TransReducer,initialTransactions)
+function addTransaction(transobj){
+    dispatch({
+        type : "ADD_TRANSACTION" ,
+        payload : {
+            amount:transobj.amount , 
+            desc:transobj.desc
+
+
+        },
+    
+    })
+
+
+}
+return(
+<TransactionContext.Provider value={{
+    transactons:state,
+    addTransaction
+
+}}>
+    {children}
+
+</TransactionContext.Provider>
+)
+}
